5407e9c6e339387dd87b57cdfd1ab7f6cc641dfb
Adarsh <adarsh71586@gmail.com>
2024-04-23 00:38:09 +0530

    added the features

--- mainTB.v
+++ mainTB.v
@@ -1,10 +1,10 @@
+`include "main.v"
 module main_tb;
 
   reg [2:0] opcode;
   reg [7:0] A, B;
   wire [7:0] Y;
-  reg [7:0] mem [0:2]; // Declare a memory
-
+  reg [7:0] mem [0:2]; 
   main uut (
     .opcode(opcode),
     .A(A),
@@ -18,10 +18,10 @@ module main_tb;
 
     $monitor("opcode = %b, A = %d, B = %d, Y = %d\n", opcode, A, B, Y);
 
-    // User input for opcode, A, and B from a file
+   
     $readmemb("input.txt", mem);
 
-    opcode = mem[0][2:0]; // Assign the values to opcode, A, and B
+    opcode = mem[0][2:0]; 
     A = mem[1];
     B = mem[2];
 
