c24402f00922dd8c5c8f582da7926beb315ba5d5
Adarsh <adarsh71586@gmail.com>
2024-04-23 00:53:40 +0530

    final testing before finals

--- main.v
+++ main.v
@@ -6,14 +6,14 @@ module main (
 
 always @* begin
   case (opcode)
-    3'b000: Y = A + B; // Add
-    3'b001: Y = A - B; // Subtract
-    3'b010: Y = A & B; // AND
-    3'b100: Y = A | B; // OR
-    3'b011: Y = A ^ B; // XOR
-    3'b101: Y = ~(A & B); // NAND
-    3'b110: Y = ~(A | B); // NOR
-    3'b111: Y = ~(A ^ B); // XNOR
+    3'b000: Y = A + B; 
+    3'b001: Y = A - B;
+    3'b010: Y = A & B; 
+    3'b100: Y = A | B;
+    3'b011: Y = A ^ B;
+    3'b101: Y = ~(A & B); 
+    3'b110: Y = ~(A | B); 
+    3'b111: Y = ~(A ^ B); 
     default: Y = 8'b0;
   endcase
 end
